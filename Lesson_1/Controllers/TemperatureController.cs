a3a87b0e738c9b6f3169e2d1263ca51505f9b2b2
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-06-07 16:12:33 +0300

    Урок 1
    Решение

---
+++ Lesson_1/Controllers/TemperatureController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson_1.Controllers
+{
+    [Route("api/temperature")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly ValuesHolder _holder;
+
+        public TemperatureController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string date, [FromQuery] int temperature)
+        {
+            try
+            {
+                _holder.Add(date, temperature);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] string dateFrom, [FromQuery] string dateTo)
+        {
+            try
+            {
+                return Ok(_holder.Get(dateFrom, dateTo));
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return Ok("welcome");
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string date, [FromQuery] int temperature)
+        {
+            try
+            {
+                _holder.Update(date, temperature);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string date)
+        {
+            try
+            {
+                _holder.Delete(date);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Lesson_1/ValuesHolder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_1
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int Temperature { get; set; }
+        public WeatherForecast(string _date, int _temperature)
+        {
+            Date = Convert.ToDateTime(_date);
+            Temperature = _temperature;
+        }
+    }
+    public class ValuesHolder
+    {
+        public List<WeatherForecast> Values { get; set; }
+        public ValuesHolder()
+        {
+            Values = new List<WeatherForecast>();
+        }
+        public void Add(string date, int temperature)
+        {
+            foreach(WeatherForecast value in Values)
+            {
+                if (value.Date == Convert.ToDateTime(date))
+                {
+                    throw new Exception($"Ошибка: дата {date} уже существует.");
+                }
+            }
+            Values.Add(new WeatherForecast(date, temperature));
+        }
+        public object Get(string dateFrom, string dateTo)
+        {
+            var list = new List<WeatherForecast>();
+            foreach (WeatherForecast value in Values)
+            {
+                if (value.Date >= Convert.ToDateTime(dateFrom) && value.Date <= Convert.ToDateTime(dateTo))
+                {
+                    list.Add(value);
+                }
+            }
+            if (list.Count > 0)
+            {
+                return list;
+            }
+            throw new Exception($"Ошибка: данных за промежуток {dateFrom}-{dateTo} не существует.");
+        }
+        public void Update(string date, int temperature)
+        {
+            foreach (WeatherForecast value in Values)
+            {
+                if (value.Date == Convert.ToDateTime(date))
+                {
+                    value.Temperature = temperature;
+                    return;
+                }
+            }
+            throw new Exception($"Ошибка: данных за дату {date} не существует.");
+        }
+        public void Delete(string date)
+        {
+            foreach (WeatherForecast value in Values)
+            {
+                if (value.Date == Convert.ToDateTime(date))
+                {
+                    Values.Remove(value);
+                    return;
+                }
+            }
+            throw new Exception($"Ошибка: данных за дату {date} не существует.");
+        }
+    }
+}
